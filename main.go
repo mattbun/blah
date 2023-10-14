d17146957befc4ce924f0a99bcb2eb06e245e602
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2023-10-14 15:05:55 -0400

    feat: allow individual lines to be set using integer flags

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	"github.com/asticode/go-astisub"
+	"flag"
+	"fmt"
 	"os"
+
+	"github.com/asticode/go-astisub"
 )
 
+const intFlagCount = 10
+
 func main() {
-	if len(os.Args) < 4 {
-		panic("Usage: blah './path/to/subtitles.src.srt' 'blah blah' './path/to/subtitles.dst.srt'")
+	input := flag.String("i", "", "input subtitle file")
+	output := flag.String("o", "", "output subtitle file")
+	text := flag.String("t", "", "text to replace every line with")
+	intFlags := parseIntFlags()
+
+	flag.Parse()
+
+	firstIndex, lastIndex := findFirstAndLastLines(intFlags)
+	if *input == "" || *output == "" || (*text == "" && firstIndex < 0 && lastIndex < 0) {
+		flag.Usage()
+		os.Exit(1)
 	}
 
-	input := os.Args[1]
-	text := os.Args[2]
-	output := os.Args[3]
-
-	subtitles, err := astisub.OpenFile(input)
-
+	subtitles, err := astisub.OpenFile(*input)
 	if err != nil {
 		panic(err)
 	}
@@ -23,16 +32,64 @@ func main() {
 	// Using the index because the value is copied
 	for subtitleIndex := range subtitles.Items {
 		subtitle := subtitles.Items[subtitleIndex]
-		for lineIndex := range subtitle.Lines {
-			line := subtitle.Lines[lineIndex]
-			for lineItemIndex := range line.Items {
-				subtitles.Items[subtitleIndex].Lines[lineIndex].Items[lineItemIndex].Text = text
+
+		if *text != "" {
+			for lineIndex := range subtitle.Lines {
+				line := subtitle.Lines[lineIndex]
+
+				for lineItemIndex := range line.Items {
+					subtitles.Items[subtitleIndex].Lines[lineIndex].Items[lineItemIndex].Text = *text
+				}
 			}
+		} else {
+			lines := []astisub.Line{}
+
+			for _, flag := range intFlags[firstIndex : lastIndex+1] {
+				lines = append(lines, astisub.Line{
+					Items: []astisub.LineItem{
+						{
+							Text:        *flag,
+							InlineStyle: subtitle.Lines[0].Items[0].InlineStyle,
+							Style:       subtitle.Lines[0].Items[0].Style,
+						},
+					},
+				})
+			}
+
+			subtitles.Items[subtitleIndex].Lines = lines
 		}
 	}
 
-	err = subtitles.Write(output)
+	err = subtitles.Write(*output)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func findFirstAndLastLines(list []*string) (int, int) {
+	firstIndex := -1
+	lastIndex := -1
+
+	for index, value := range list {
+		if *value != "" {
+			if firstIndex < 0 {
+				firstIndex = index
+			}
+
+			lastIndex = index
+		}
+	}
+
+	return firstIndex, lastIndex
+}
+
+func parseIntFlags() []*string {
+	flags := []*string{}
+	for i := 0; i < intFlagCount; i++ {
+		flags = append(
+			flags,
+			flag.String(fmt.Sprintf("%d", i), "", fmt.Sprintf("What to put in line %d", i)),
+		)
+	}
+	return flags
 }
